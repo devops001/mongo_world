97e4345ef7de1c8888e19d181a3d0164ef4840bd
devops001 <devops001@gmail.com>
2015-01-06 07:51:48 -0800

    organized client; reporting cmd errors instead of crashing

--- lib/client.rb
+++ lib/client.rb
@@ -5,10 +5,109 @@ require_relative 'db'
 
 class Client
 
+  ##########################################
+  ## initializing:
+  ##########################################
+
   def initialize
-    setup_data
-    setup_commands
+    initialize_data
+    initialize_commands
+    @is_running = false
   end
+
+  def initialize_data
+    @db = Db.new
+    @db.destroy_collection!('rooms')
+    @db.destroy_collection!('users')
+
+    @home = Model.new(@db, 'rooms')
+    @home.name  = 'home'
+    @home.desc  = 'a home'
+    @home.doors = [];
+    @home.save!
+
+    @user = Model.new(@db, 'users')
+    @user.name    = 'user'
+    @user.desc    = 'a user'
+    @user.room_id = @home._id
+    @user.save!
+
+    update_room!
+  end
+
+  def initialize_commands
+    @cmd = {
+      'exit'  => lambda { @is_running=false },
+      'clear' => lambda { puts `clear` },
+      'room'  => lambda { puts @room },
+      'save'  => lambda { |name='default'| save(name) },
+      'ls_saves'  => lambda { ls_saves },
+      'load_save'   => lambda { |name='default'| load_save(name) },
+      'rm_save'     => lambda { |name| rm_save(name) },
+      'debug' => lambda {
+        print "debug is ".colorize(:light_black)
+        puts @db.toggle_debug ? "on".colorize(:light_green) : "off".colorize(:light_red)
+      },
+      'ls'    => lambda {
+        doors = @room.doors.map { |door| door['room_name'] }.join(', ').colorize(:light_blue)
+        puts @room.name.colorize(:light_blue) +": ".colorize(:light_black) + @room.desc.colorize(:white)
+        puts "doors: [".colorize(:light_black) + doors +"]".colorize(:light_black)
+      },
+      'cd' => lambda { |room_name=nil|
+        if room_name.nil?
+          next_room_id = @home._id
+        else
+          next_room_id = nil
+          @room.doors.each do |door|
+            if door['room_name'] == room_name
+              next_room_id = door['room_id']
+              break
+            end
+          end
+        end
+        if next_room_id
+          @room = Model.new(@db, 'rooms', next_room_id)
+          @user.room_id = next_room_id
+          @user.save!
+        else
+          puts "there is no door for \"#{room_name}\""
+        end
+      },
+      'mkdir' => lambda { |name, desc='a room'|
+        room = Model.new(@db, 'rooms')
+        room.name  = name
+        room.desc  = desc
+        room.doors = [ {'room_name'=>@room.name, 'room_id'=>@room._id} ]
+
+        exists = false
+        @room.doors.each do |existing_door|
+          if existing_door['room_name'] == room.name
+            puts "INFO: ".colorize(:light_black) + " saving over existing room ".colorize(:light_cyan) + room.name.colorize(:light_blue)
+            room._id = existing_door['room_id']
+            exists   = true
+          end
+        end
+
+        room.save!
+        if not exists
+          @room.doors << {'room_id'=>room._id, 'room_name'=>room.name}
+          @room.save!
+        end
+      },
+      'desc' => lambda { |new_description|
+        @room.desc = new_description
+        @room.save!
+      }
+    }
+    @cmd['quit'] = @cmd['exit']
+    @cmd['help'] = lambda {
+      puts "Commands: #{@cmd.keys.sort.join(', ')}"
+    }
+  end
+
+  ##########################################
+  ## saving
+  ##########################################
 
   def save (saved_name)
     @saved = Model.new(@db, 'saves', find_save_id(saved_name))
@@ -68,95 +167,9 @@ class Client
     end
   end
 
-  def setup_data
-    @db = Db.new
-    @db.destroy_collection!('rooms')
-    @db.destroy_collection!('users')
-
-    @home = Model.new(@db, 'rooms')
-    @home.name  = 'home'
-    @home.desc  = 'a home'
-    @home.doors = [];
-    @home.save!
-    
-    @user = Model.new(@db, 'users')
-    @user.name    = 'user'
-    @user.desc    = 'a user' 
-    @user.room_id = @home._id
-    @user.save!
-
-    update_room!
-  end
-
-  def setup_commands
-    @cmd = {
-      'exit'  => lambda { exit 0 },
-      'clear' => lambda { puts `clear` },
-      'room'  => lambda { puts @room },
-      'save'  => lambda { |name='default'| save(name) },
-      'ls_saves'  => lambda { ls_saves },
-      'load_save'   => lambda { |name='default'| load_save(name) },
-      'rm_save'     => lambda { |name| rm_save(name) },
-      'debug' => lambda { 
-        print "debug is ".colorize(:light_black)
-        puts @db.toggle_debug ? "on".colorize(:light_green) : "off".colorize(:light_red)
-      },
-      'ls'    => lambda { 
-        doors = @room.doors.map { |door| door['room_name'] }.join(', ').colorize(:light_blue)
-        puts @room.name.colorize(:light_blue) +": ".colorize(:light_black) + @room.desc.colorize(:white)
-        puts "doors: [".colorize(:light_black) + doors +"]".colorize(:light_black)
-      },
-      'cd' => lambda { |room_name=nil|
-        if room_name.nil?
-          next_room_id = @home._id
-        else
-          next_room_id = nil
-          @room.doors.each do |door|
-            if door['room_name'] == room_name
-              next_room_id = door['room_id'] 
-              break
-            end
-          end
-        end
-        if next_room_id
-          @room = Model.new(@db, 'rooms', next_room_id)
-          @user.room_id = next_room_id
-          @user.save!
-        else
-          puts "there is no door for \"#{room_name}\""
-        end
-      },
-      'mkdir' => lambda { |name, desc='a room'|
-        room = Model.new(@db, 'rooms')
-        room.name  = name
-        room.desc  = desc
-        room.doors = [ {'room_name'=>@room.name, 'room_id'=>@room._id} ]
-
-        exists = false
-        @room.doors.each do |existing_door|
-          if existing_door['room_name'] == room.name
-            puts "INFO: ".colorize(:light_black) + " saving over existing room ".colorize(:light_cyan) + room.name.colorize(:light_blue)
-            room._id = existing_door['room_id']
-            exists   = true
-          end
-        end
-
-        room.save!
-        if not exists
-          @room.doors << {'room_id'=>room._id, 'room_name'=>room.name}
-          @room.save!
-        end
-      },
-      'desc' => lambda { |new_description|
-        @room.desc = new_description
-        @room.save!
-      }
-    }
-    @cmd['quit'] = @cmd['exit']
-    @cmd['help'] = lambda {
-      puts "Commands: #{@cmd.keys.sort.join(', ')}"
-    }
-  end
+  ##########################################
+  ## other:
+  ##########################################
 
   def commands
     @cmd
@@ -175,17 +188,23 @@ class Client
   end
 
   def run
+    @is_running = true
     begin
-      loop do
+      while @is_running do
         update_room!
         $stdout.print prompt
         line = $stdin.gets.strip
         cmd, *args = Shellwords.shellsplit(line)
         next if cmd.nil?
         if @cmd[cmd]
-          @cmd[cmd].call(*args)
+          begin
+            @cmd[cmd].call(*args)
+          rescue Exception => e
+            puts "command failed: ".colorize(:light_red) + line
+            puts "  #{e.to_s}"
+          end
         else
-          puts "unknown command: #{line}"
+          puts "unknown command: ".colorize(:light_red) + line
         end
       end
     ensure
