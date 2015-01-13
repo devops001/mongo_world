f856ccf7fdb1720d80f42394b8177e495b3f22e9
devops001 <devops001@gmail.com>
2015-01-13 08:25:33 -0800

    client tests

--- test/client_test.rb
+++ test/client_test.rb
@@ -12,7 +12,7 @@ class ClientTest < Minitest::Test
   end
 
   def teardown
-    @world.destroy_collections!
+    @world.destroy_database!
   end
 
   def test_colorize_markdown input1 = '[blue]one[/blue]'
@@ -33,6 +33,69 @@ class ClientTest < Minitest::Test
   def test_new
     assert_equal(World, @world.class)
     assert_equal(Hash,  @cmd.class)
+  end
+
+  def test_echo
+    out, err = capture_io do
+      @client.echo("something")
+    end
+    assert_equal("", out)
+    assert_equal("", err)
+
+    @client.instance_variable_set(:@use_stdout, true)
+    out, err = capture_io do
+      @client.echo("something")
+    end
+    assert_equal("something\n", out)
+    assert_equal("", err)
+
+    @client.instance_variable_set(:@use_stdout, false)
+  end
+
+  def test_prompt
+    expect = "\e[0;95;49muser\e[0m\e[0;90;49m@\e[0m\e[0;94;49mhome\e[0m\e[0;90;49m> \e[0m"
+    assert_equal(expect, @client.prompt)
+
+    @cmd['mkdir'].call('room', 'a room')
+    @cmd['cd'].call('room')
+
+    expect = "\e[0;95;49muser\e[0m\e[0;90;49m@\e[0m\e[0;94;49mroom\e[0m\e[0;90;49m> \e[0m"
+    assert_equal(expect, @client.prompt)
+
+    @cmd['touch'].call('book', 'a book')
+    @cmd['make'].call('dog', 'a dog')
+    @cmd['mkdir'].call('pond', 'a pond')
+
+    expect = "\e[0;95;49muser\e[0m\e[0;90;49m@\e[0m\e[0;94;49mroom\e[0m\e[0;90;49m> \e[0m"
+    assert_equal(expect, @client.prompt)
+  end
+
+  def test_update_current_room!
+    room = @client.instance_variable_get(:@room)
+
+    assert_equal('home', room.name)
+    assert_equal(0, room.doors.count)
+    assert_equal(0, room.items.count)
+    assert_equal(0, room.mobs.count)
+
+    r = Model.new(@db, 'rooms', room._id)
+    r.items << @world.create_item_data('book', 'a book')
+    r.mobs  << @world.create_mob_data('dog', 'a dog')
+    r.save!
+
+    room = @client.instance_variable_get(:@room)
+    assert_equal('home', room.name)
+    assert_equal(0, room.doors.count)
+    assert_equal(0, room.items.count)
+    assert_equal(0, room.mobs.count)
+
+    @client.update_current_room!
+
+    room = @client.instance_variable_get(:@room)
+    assert_equal('home', room.name)
+    assert_equal(0, room.doors.count)
+    assert_equal(1, room.items.count)
+    assert_equal(1, room.mobs.count)
   end
 
 end
